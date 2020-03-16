0612214abf61146d03766b61639e1be2102a2f26
Jorge Luis Perales <jorgeluis070294@hotmail.com>
2020-03-16 00:28:20 -0600

    Fix issue with user login route

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,7 +24,7 @@ router.post('/users/login', async (req, res) => {
         const token = await user.generateAuthToken()
         res.send({ user, token })
     } catch (e) {
-        res.send(400).send(e)
+        res.status(400).send(e)
     }
 })
 
